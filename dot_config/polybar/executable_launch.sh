31733f9cf28c9056ac7e54b538a7e83bc2d26635
epbuennig <inquiries@epbuennig.me>
2022-08-11 11:02:58 +0200

    updated wm config

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,13 +1,10 @@
-#!/bin/bash
+#! /bin/sh
 
-# kill all previous
 killall -q polybar
-while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+pgrep -x polybar > /dev/null && exit 1
 
-# always start main
 polybar top &
 
-# start side if connected
 if [[ $(xrandr -q | grep 'HDMI-0 connected' ) ]]; then
     polybar top-side &
 fi
---
+++ dot_config/sxhkd/executable_launch.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+killall -q sxhkd
+pgrep -x sxhkd > /dev/null || sxhkd &
